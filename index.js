6bb64e1b005357e2a57f062663fd491fb447e650
brayan <brayantino24@hotmail.com>
2019-05-24 09:36:19 -0500

    terminado

--- index.js
+++ index.js
@@ -4,26 +4,16 @@ var exphbs = require('express-handlebars');
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
-// Connection URL
 const url = 'mongodb://localhost:27017';
-
-// Database Name
-const dbName = 'tienda';
-
-// Create a new MongoClient
+const dbName = 'visitas';
 const client = new MongoClient(url);
-
 var db = null;
 
-// Use connect method to connect to the Server
 client.connect(function (err) {
     assert.equal(null, err);
 
     db = client.db(dbName);
-
-    //client.close();
 });
-
 
 var app = express();
 
@@ -36,23 +26,80 @@ app.engine('handlebars', exphbs({
 app.set('view engine', 'handlebars');
 
 app.get('/inicio', function (request, response) {
- 
+    var visita = {
+        nombre:'Inicio',
+        fecha: new Date(),
+
+    };
+
+    var collection = db.collection('visitas');
+    collection.insertOne(visita, function(err){
+        assert.equal(err, null);
+
+        console.log('pedido guardado');
+    });
     response.render('inicio');
 });
 
 app.get('/sobre', function (request, response) {
- 
+    var visita = {
+        nombre:'Sobre',
+        fecha: new Date(),
+
+    };
+
+    var collection = db.collection('visitas');
+    collection.insertOne(visita, function(err){
+        assert.equal(err, null);
+
+        console.log('pedido guardado');
+    });
     response.render('sobre');
 });
 
 app.get('/contacto', function (request, response) {
- 
-    response.render('contacto');
+    var visita = {
+        nombre:'Contacto',
+        fecha: new Date(),
+
+    };
+
+    var collection = db.collection('visitas');
+    collection.insertOne(visita, function(err){
+        assert.equal(err, null);
+
+        console.log('pedido guardado');
+    });    response.render('contacto');
 });
 
-app.get('/admin', function(req, res) {
-   
-    res.render('admin');
+app.get('/admin', function(request, response) {
+    client.connect(function (err) {
+        assert.equal(null, err);
+        const db = client.db(dbName);
+        const visitas = db.collection('visitas');
+        visitas.find({}).sort({_id:-1}).toArray(function (err, docs) {
+            assert.equal(err, null);
+            let visitasIni = 0;
+            let  visitasSobre = 0;
+            let visitasConta = 0;
+            docs.forEach(visita => {
+                if (visita.nombre == 'Inicio') {
+                    visitasIni += 1;
+                } else if (visita.nombre == 'Sobre') {
+                    visitasSobre += 1;
+                } else if (visita.nombre == 'Contacto') {
+                    visitasConta += 1;
+                } 
+            });
+            contexto = {
+                visitaIni: visitasIni,
+                visitaSobre: visitasSobre,
+                visitaConta: visitasConta,
+                visitas: docs
+            };
+            response.render('admin', contexto);
+        });
+    });
 });
 
 app.listen(3000);
